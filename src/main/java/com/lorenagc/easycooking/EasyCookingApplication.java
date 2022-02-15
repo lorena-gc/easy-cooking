07b06e33ea526638f2663b43daba76963a98e0ac
Lorena Gomes <lorenag@ciandt.com>
2022-02-15 15:12:21 -0300

    Initial commit

---
+++ src/main/java/com/lorenagc/easycooking/EasyCookingApplication.java
@@ -0,0 +1,13 @@
+package com.lorenagc.easycooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lorenagc/easycooking/EasyCookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenagc.easycooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
